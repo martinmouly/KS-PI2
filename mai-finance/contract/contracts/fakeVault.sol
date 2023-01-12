c1dc9f2c3c4425f9a7d04d72975ba8c978a3eda0
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-01-12 11:32:33 +0100

    mai-finance : update fakeVault

--- mai-finance/contract/contracts/fakeVault.sol
+++ mai-finance/contract/contracts/fakeVault.sol
@@ -17,15 +17,13 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract fakeMaiVault{
 
-    mapping (address => bool) public admin; // nous
     mapping (uint256 => address) public owners; // associe un nft a son owner
     mapping (address => uint256) public erc721Balance; // balance de nft d'un owner
     mapping(uint256 => uint256) public _deposit; // deposit dans le vault : vault => montant total déposé
     mapping(uint256 => uint256) public maiDebt; // montant de mai emprunté par une vault : vault => debt
+    mapping(address => uint256) public maiBalance; // monta de may détenu par chaque addresse
 
-    constructor(){
-        admin[msg.sender] = true;
-    }
+    uint256 vaultCount = 0;
 
     function ownerOf(uint256 _tokenId) public view returns (address) {
         return owners[_tokenId];
@@ -46,6 +44,84 @@ contract fakeMaiVault{
         require(owners[_erc721_Id] == msg.sender, "You are not the owner of this NFT");
         _deposit[_erc721_Id] += amount;
     }
+
+    function Transfer(address _to, uint256 _erc721_Id) public {
+        require(owners[_erc721_Id] == msg.sender, "You are not the owner of this NFT");
+        owners[_erc721_Id] = _to;
+        erc721Balance[msg.sender] -= 1;
+        erc721Balance[_to] += 1;
+    }
+
+    function TransferFrom(address from, address to, uint256 amount) public {
+        require(owners[_erc721_Id] == msg.sender, "You are not the owner of this NFT");
+        owners[_erc721_Id] = _to;
+        erc721Balance[from] -= 1;
+        erc721Balance[_to] += 1;
+    }
+
+    function TransferMai(address _to, uint256 amount) public {
+        require(maiBalance[msg.sender] >= amount, "You don't have enough mai");
+        maiBalance[msg.sender] -= amount;
+        maiBalance[_to] += amount;
+    }
+
+    function TransferMaiFrom(address from, address to, uint256 amount) public {
+        require(maiBalance[from] >= amount, "You don't have enough mai");
+        maiBalance[from] -= amount;
+        maiBalance[_to] += amount;
+    }
+
+    function updateVaultDebt(uint256 vaultId) public view returns (uint256) {
+        return maiDebt[vaultId];
+    }
+
+    function payBackToken( uint256 vaultID, uint256 amount, uint256 _front) public {
+        require(owners[vaultID] == msg.sender, "You are not the owner of this vault");
+        require(maiDebt[vaultID] >= amount, "You don't have enough debt");
+        require(maiBalance[msg.sender] >= amount, "You don't have enough mai");
+        maiDebt[vaultID] -= amount;
+        maiBalance[msg.sender] -= amount;
+    }
+
+    function depositCollateral(uint256 vaultID, uint256 amount) public {
+        require(owners[vaultID] == msg.sender, "You are not the owner of this vault");
+        _deposit[vaultID] += amount;
+    }
+
+    function withdrawCollateral(uint256 vaultID, uint256 amount) public {
+        require(owners[vaultID] == msg.sender, "You are not the owner of this vault");
+        require(_deposit[vaultID] >= amount, "You don't have enough collateral");
+        require(maiDebt[vaultID] * (1/0.8) <= _deposit[vaultID] - amount, "You don't have enough collateral");
+        _deposit[vaultID] -= amount;
+        maiBalance[msg.sender] += amount;
+    }
+
+    function newVault() public{
+        vaultCount += 1;
+        owners[vaultCount] = msg.sender;
+        erc721Balance[msg.sender] += 1;
+    }
+
+
+    // view
+    function getMaiBalance(address _owner) public view returns (uint256) {
+        return maiBalance[_owner];
+    }
+    function getVaultDebt(uint256 _vaultId) public view returns (uint256) {
+        return maiDebt[_vaultId];
+    }
+    function getVaultCollateral(uint256 _vaultId) public view returns (uint256) {
+        return _deposit[_vaultId];
+    }
+    function getOwner(uint256 _vaultId) public view returns (address) {
+        return owners[_vaultId];
+    }
+
+    //faucet
+    function getMai(uint256 _amount) public {
+        maiBalance[msg.sender] += _amount;
+    }
+
 
     
 }
