ac478d16d3338db4ff56ce262c2fd049493658de
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-01-14 17:40:19 +0100

    mai-finance : add interfaces

--- mai-finance/contract/contracts/fakeVault.sol
+++ mai-finance/contract/contracts/fakeVault.sol
@@ -14,6 +14,32 @@ pragma solidity ^0.7.0; // regarder la version sur les contracts de qidao 0.5.5 
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol@v4.6.0";
+
+contract VaultNFTv4 is ERC721, ERC721Enumerable {
+
+    string public uri;
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    constructor(string memory name, string memory symbol, string memory _uri)
+        ERC721(name, symbol)
+    {
+        uri = _uri;
+    }
+
+    function tokenURI(uint256 tokenId) public override view returns (string memory) {
+        require(_exists(tokenId));
+
+        return uri;
+    }
+}
 
 contract fakeMaiVault{
 
