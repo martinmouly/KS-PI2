b640fd30aff711c02097ec5456164fbe89978cea
Lucas Kaczmarski <lucaskacz1@gmail.com>
2023-01-19 15:33:27 +0100

    Add Counter Contract and can now Create Task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: process.env.PROVIDER_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  }
 };
--- src/pages/Automate.js
+++ src/pages/Automate.js
@@ -1,11 +1,56 @@
 import { useState } from "react";
+import { GelatoOpsSDK, isGelatoOpsSupported, TaskTransaction } from "@gelatonetwork/ops-sdk";
+import { Contract } from "ethers";
+import { ethers } from "ethers";
+import counterAbi from "../abis/CounterTest.json";
 
 function Automate(){
-    return(
+
+    async function CreateTask(){
+        // const providerUrl = "https://eth-goerli.g.alchemy.com/v2/"
+        // let provider = ethers.getDefaultProvider("goerli");
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+
+        const networkDetails = await provider.getNetwork();
+        const chainId = networkDetails.chainId;
+
+        // let signer = provider.getSigner();
+
+
+        if (!isGelatoOpsSupported(chainId)) {
+            console.log(`Gelato Ops network not supported (${chainId})`);
+            return;
+        }
+
+        const gelatoOps = new GelatoOpsSDK(chainId, signer);
+        const counter = new Contract("0x69C904fed91BFe2a354DbDd1A1078485389Fe6E9", counterAbi, signer);
+        const selector = counter.interface.getSighash("increaseCount(uint256)");
+        const resolverData = counter.interface.getSighash("checker()");
+
+            // Create task
+            console.log("Creating Task...");
+            const { taskId, tx } = await gelatoOps.createTask({
+                execAddress: counter.address,
+                execSelector: selector,
+                execAbi: JSON.stringify(counterAbi),
+                resolverAddress: counter.address,
+                resolverData: resolverData,
+                resolverAbi: JSON.stringify(counterAbi),
+                name: "Automated counter with resolver",
+                dedicatedMsgSender: true,
+            });
+            await tx.wait();
+            console.log(`Task created, taskId: ${taskId} (tx hash: ${tx.hash})`);
+            console.log(`> https://app.gelato.network/task/${taskId}?chainId=${chainId}`);   
+    }
+
+        return(
         <div className="automation">
-            Hello
+            <button onClick={CreateTask}>Create Task</button>
         </div>
     );
 }
 
 export default Automate;
+
